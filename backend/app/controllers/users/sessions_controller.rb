b15162de7e15503f4ac970f68bead75775090a20
adham90 <adham_eldeeb90@hotmail.com>
2015-03-18 22:38:10 +0200

    ignore

--- backend/app/controllers/users/sessions_controller.rb
+++
@@ -1,5 +0,0 @@
-class Users::SessionsController < ApplicationController
-  def new
-    puts "sessions new func"
-  end
-end
--- backend/config/initializers/codeclimate.rb
+++
@@ -1,4 +0,0 @@
-CodeClimate::TestReporter.configure do |config|
-  config.path_prefix = "backend" #the root of your Rails application relative to the repository root
-  config.git_dir = "../" #the relative or absolute location of your git root compared to where your tests are run
-end
